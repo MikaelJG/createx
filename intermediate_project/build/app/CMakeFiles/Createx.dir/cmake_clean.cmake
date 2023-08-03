248ac97f28f377ee54aed632fd1557d068ca005c
MikaelJG <mjgonsalves@protonmail.com>
2023-08-03 17:30:08 -0400

    an cmake project

---
+++ intermediate_project/build/app/CMakeFiles/Createx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Createx.dir/main.cc.o"
+  "Createx"
+  "Createx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Createx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
