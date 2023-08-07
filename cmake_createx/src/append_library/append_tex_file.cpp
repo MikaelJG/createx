3959e8c9f85ae8d17f9c9c3a168d34038d564649
MikaelJG <mjgonsalves@protonmail.com>
2023-08-07 09:36:01 -0400

    appending new tex file

--- cmake_createx/src/append_library/append_tex_file.cpp
+++ cmake_createx/src/append_library/append_tex_file.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
-// #include <filesystem>
-// #include <fstream>
+#include <filesystem>
+#include <fstream>
 #include <vector>
 #include "append_tex_file.h"
 
-// namespace fs = std::filesystem;
+namespace fs = std::filesystem;
 
 void append_tex_file(std::vector<std::string> files_to_append, std::string output_path_string) {
+    try {
 
-    std::cout << "Output is: " << output_path_string << '\n';
+        std::cout << "Output is: " << output_path_string << '\n';
 
-    for (const auto& f : files_to_append) {
-        std::cout << "f is: " << f << '\n';
-        
-        // std::ifstream input_file("file_1.txt");
-        // std::ofstream output_file("file.tex", std::ios::app); // Open in append mode
+        for (const auto& f : files_to_append) {
+            std::cout << "f is: " << f << '\n';
+            
+            std::ifstream input_file(f);
+            std::ofstream output_file(output_path_string, std::ios::app); // Open in append mode
 
-        // if (input_file.is_open() && output_file.is_open()) {
-        //     std::string line;
-        //     while (std::getline(input_file, line)) {
-        //         output_file << line << '\n'; // Append each line to the output file
-        //     }
+            if (input_file.is_open() && output_file.is_open()) {
+                std::string line;
+                while (std::getline(input_file, line)) {
+                    output_file << line << '\n'; // Append each line to the output file
+                }
 
-        //     std::cout << "File contents successfully appended." << std::endl;
-        // } else {
-        //     std::cerr << "Failed to open files." << std::endl;
-        // }
+                std::cout << "File contents successfully appended." << std::endl;
+            } else {
+                std::cerr << "Failed to open files." << std::endl;
+            }
 
-        // input_file.close();
-        // output_file.close();
+            input_file.close();
+            output_file.close();
+        }
+    }
+    catch (const fs::filesystem_error& unappendable_files) {
+        const std::string error_message = unappendable_files.what();
+        for (const char& c : error_message) {
+            std::cout << c;
+            if (c == ':') {
+                std::cout << '\n'; // Print newline after each space character
+            }
+        }
+        std::cout << '\n';
 
+        // Filesystem crashed when searching wanted dir
+        std::cout << "Files could not be appeneded." << '\n';
+        std::cout << "Exiting program..." << '\n';
+        std::exit(0);
     }
 }    
