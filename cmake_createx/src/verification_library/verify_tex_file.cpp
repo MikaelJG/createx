7645ea460177b5aeef015b124adfb3d1ddbcf06b
MikaelJG <mjgonsalves@protonmail.com>
2023-08-06 20:47:12 -0400

    adding function in lib

---
+++ cmake_createx/src/verification_library/verify_tex_file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+void verify_tex_file(fs::path output_file) {
+
+    // the output file might me misplaced
+    
+    if (fs::is_regular_file(output_file)) {
+        std::cout << "Output file created." << '\n';
+        std::cout << '\n';
+    } else {
+        std::cout << "Failed to create output file." << '\n';
+        std::cout << "Exiting program..." << '\n';
+        std::exit(0);
+    } 
+}
---
+++ cmake_createx/src/verification_library/verify_tex_file.h
@@ -0,0 +1,5 @@
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+void verify_tex_file(fs::path output_file);
